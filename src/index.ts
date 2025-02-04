0e4e30cdc7c6cece891050de89d2e1756cc21abd
Avneet Kaur <avibal1325@gmail.com>
2025-02-04 20:29:16 +0530

    Signup Signin and Content route Added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,71 @@
-import express from "express";
+import express, { json } from "express";
+import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
 
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: Add zod validation, hash password
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel .create({
+            username: username,
+            password: password
+        });
+    
+        res.status(200).json({
+            message: "User signed up"
+        })
+    } catch(error) {
+        res.status(411).json({
+            error: "User already exits"
+        })
+    }
 })
 
-app.post("/api/v1/signin", (req, res) => {
-    
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            error: "Incorrect Credentials"
+        })
+    }
 })
 
-app.post("/api/v1/content", (req, res) => {
-    
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        //@ts-ignore
+        userId: req.userId,
+        tags: []
+    })
+
+    res.json({
+        message: "Content Added"
+    })
 })
 
 app.get("/api/v1/signup", (req, res) => {
