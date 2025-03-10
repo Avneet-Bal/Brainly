1da85a00509ec8c381919c4e90a23a393f9005a5
Avneet Kaur <avibal1325@gmail.com>
2025-03-11 04:42:39 +0530

    share route is added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { json } from "express";
 import jwt from "jsonwebtoken";
-import { ContentModel, UserModel } from "./db";
+import { ContentModel, LinkModel, UserModel } from "./db";
 import { JWT_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
+import { random } from "./utils";
 
 const app = express();
 app.use(express.json());
@@ -93,8 +94,24 @@ app.delete("/api/v1/content", userMiddleware, async (req, res) => {
     })
 })
 
-app.post("/api/v1/brain/share", (req, res) => {
-    
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    if (share) {
+        await LinkModel.create({
+            //@ts-ignore
+            userId: req.userId,
+            hash: random(10)
+        })
+    } else {
+        await LinkModel.deleteOne({
+            //@ts-ignore
+            userId: req.userId
+        })
+    }
+
+    res.json({
+        message: "Updated shareable link"
+    })
 })
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {
